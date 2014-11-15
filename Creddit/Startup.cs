8567e42710d3f84f1be1b84e7faf03fd3b6a4413
Saad Bakht <rotk13@gmail.com>
2014-11-15 18:08:45 -0500

    initial files

---
+++ Creddit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Creddit.Startup))]
+namespace Creddit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
